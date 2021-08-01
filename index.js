c4de3d958f669318ceb7bc45541290215b708b82
spicyzboss <bossthth1209mine@gmail.com>
2021-08-02 01:31:38 +0700

    :beetle: fix speak send form bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var synth = window.speechSynthesis;
 speak = () => {
-    var x = document.querySelector("#text").value
+    var x = document.querySelector("#text").value;
     var utterThis = new SpeechSynthesisUtterance(x);
     utterThis.lang = "th-TH";
     synth.speak(utterThis);
